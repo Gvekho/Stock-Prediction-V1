ae2b6466404742ddbabe0ed81d641e7c5d3abb4c
Giorgi Gvekhidze <92603830+Gvekho@users.noreply.github.com>
2023-08-27 00:42:51 +0400

    app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import yfinance as yf
 from prophet import Prophet
 from prophet.plot import plot_plotly
 from plotly import graph_objects as go
+from streamlit import SessionState
 
 
 today = datetime.date.today()
